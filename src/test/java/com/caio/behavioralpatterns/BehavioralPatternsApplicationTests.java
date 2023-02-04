235345198890cb0123e329b91b61f5e8b4d1ecc8
Caio-Santana <caio.santana@live.com>
2023-02-04 11:27:39 -0300

    Initial commit

---
+++ src/test/java/com/caio/behavioralpatterns/BehavioralPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.behavioralpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BehavioralPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
